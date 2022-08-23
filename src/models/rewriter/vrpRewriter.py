f2f112378b59be139928511586d61282489dde79
yangzhe Kong <yangzhekong@sz-dl-376.autox.sz>
2022-08-23 17:11:58 +0800

    update

--- src/models/rewriter/vrpRewriter.py
+++ src/models/rewriter/vrpRewriter.py
@@ -37,3 +37,27 @@ class vrpRewriter(object):
 			res.add_route_node(new_node_idx)
 			cur_capacity = res.vehicle_state[-1][1]
 		return res
+
+class vrptwRewriter(object):
+	"""
+	Rewriter for vehicle routing.
+	"""
+	def move(self, dm, cur_route_idx, neighbor_route_idx):
+		min_update_idx = min(cur_route_idx, neighbor_route_idx)
+		res = dm.clone()
+		old_vehicle_state = res.vehicle_state[:]
+		old_vehicle_state[cur_route_idx], old_vehicle_state[neighbor_route_idx] = old_vehicle_state[neighbor_route_idx], old_vehicle_state[cur_route_idx]
+		if old_vehicle_state[neighbor_route_idx][0] == 0:
+			del old_vehicle_state[neighbor_route_idx]
+		res.vehicle_state = res.vehicle_state[:min_update_idx]
+		res.route = res.route[:min_update_idx]
+		res.tot_dis = res.tot_dis[:min_update_idx]
+		cur_node_idx, cur_capacity = res.vehicle_state[-1]
+		for t in range(min_update_idx, len(old_vehicle_state)):
+			new_node_idx, new_capacity = old_vehicle_state[t]
+			new_node = res.get_node(new_node_idx)
+			if new_node_idx != 0 and cur_capacity < new_node.demand:
+				res.add_route_node(0)
+			res.add_route_node(new_node_idx)
+			cur_capacity = res.vehicle_state[-1][1]
+		return res
