67562a194b4418b63244f6470e5a2cfa6be0c455
Yanko96 <60613593+Yanko96@users.noreply.github.com>
2022-09-06 15:02:20 +0800

    Update logger.py

--- src/models/model_utils/logger.py
+++ src/models/model_utils/logger.py
@@ -9,21 +9,26 @@ import pandas as pd
 
 
 class Logger(object):
-	"""
-	The class for recording the training process.
-	"""
-	def __init__(self, args):
-		self.log_interval = args.log_interval
-		self.log_name = os.path.join(args.model_dir, args.log_name)
-		self.best_reward = 0
-		self.records = []
-		if not os.path.exists(args.model_dir):
-			os.makedirs(args.model_dir)
+    """
+    The class for recording the training process.
+    """
+    def __init__(self, args):
+        self.log_interval = args.log_interval
+        self.log_name = os.path.join(args.model_dir, args.log_name)
+        self.records = []
+        if not os.path.exists(args.model_dir):
+            os.makedirs(args.model_dir)
 
 
-	def write_summary(self, summary):
-		print("global-step: %(global_step)d, avg-reward: %(avg_reward).3f" % summary)
-		self.records.append(summary)
-		df = pd.DataFrame(self.records)
-		df.to_csv(self.log_name, index=False)
-		self.best_reward = max(self.best_reward, summary['avg_reward'])
+    def write_summary(self, summary):
+        to_print = ""
+        for key, value in summary.items():
+            to_print += key
+            to_print += ": "
+            to_print += str(round(value, 3))
+            to_print += " "
+        print(to_print)
+        self.records.append(summary)
+        df = pd.DataFrame(self.records)
+        df.to_csv(self.log_name, index=False)
+
